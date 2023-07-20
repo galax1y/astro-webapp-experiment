ef8677490b6546716cba00f69b6d5598184e4325
LucasWermann <lucasvw.eng@gmail.com>
2023-07-20 16:54:13 -0300

    Add: Experimenting with Astro

---
+++ src/components/react/example.tsx
@@ -0,0 +1,7 @@
+export function ExampleComponent() {
+  return (
+    <div className='my-4 shadow-lg flex items-center justify-center w-full h-12 bg-emerald-400 rounded-md'>
+      <h1 className='text-lg underline font-bold text-zinc-800 animate-pulse'>Example component</h1>
+    </div>
+  )
+}
